28172ccc0b8994fe20fe70c3c2680dc598107ce6
ben-jamming-reilly <benreilly0809@gmail.com>
2021-12-11 19:09:39 -0800

    add links

--- registration/regserve/urls.py
+++ registration/regserve/urls.py
@@ -1,12 +1,31 @@
-from django.urls import path 
+from django.urls import path
 
-from . import views 
+from . import views
 
 app_name = 'regserve'
 
 urlpatterns = [
-    path('/students/', views.StudentListForm.as_view(), name="students"),
-    path('/createstudent/', views.StudentCreateForm.as_view(), name="create_students"),
-    path('/data/students/', views.StudentListCreate.as_view()),
+    path('/students/',
+         views.StudentListForm.as_view(),
+         name="students"),
+    # CRUD for student
+    # Create
+    path('/students/create',
+         views.StudentCreateForm.as_view(),
+         name="create_students"),
+    # Read
+    path('/students/<pk>/',
+         views.StudentReadView.as_view(),
+         name="student_detail"),
+    # Update
+    path('/students/<pk>/update',
+         views.StudentUpdateForm.as_view(),
+         name="student_update"),
+    # Delete
+    path('/students/<pk>/delete',
+         views.StudentDeleteForm.as_view(),
+         name="student_update"),
+    path('/data/students/',
+         views.StudentListCreate.as_view()),
     path('', views.index, name='index')
 ]
--- registration/regserve/views.py
+++ registration/regserve/views.py
@@ -3,17 +3,44 @@ from django.http import HttpResponse
 from rest_framework import generics
 from .serializers import *
 from django.views.generic import ListView, CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+
 
 def index(request):
     return HttpResponse("Hello from django backend")
+
 
 class StudentListCreate(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
 class StudentListForm(ListView):
     model = Student
 
+# CRUD for student
+
+
 class StudentCreateForm(CreateView, ListView):
     model = Student
-    fields = ['firstname', 'lastname', 'idnumber', 'schoolyear', 'major', 'gpa']
+    fields = ['firstname', 'lastname',
+              'idnumber', 'schoolyear', 'major', 'gpa']
+
+
+class StudentReadView(DetailView):
+    model = Student
+    fields = ['firstname', 'lastname',
+              'idnumber', 'schoolyear', 'major', 'gpa']
+
+
+class StudentUpdateForm(UpdateView):
+    model = Student
+    fields = ['firstname', 'lastname',
+              'idnumber', 'schoolyear', 'major', 'gpa']
+    success_url = "/regserve/students/"
+
+
+class StudentDeleteForm(DeleteView):
+    model = Student
+    success_url = "/regserve/students/"
